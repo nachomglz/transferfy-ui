5e9401de246fd497ce034993061e1e704c4205b5
nachomglz <nachopad3@gmail.com>
2024-07-21 01:38:46 +0200

    init-project
    added shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <Button>Login</Button>
+    </>
+  );
+}
+
+export default App;
